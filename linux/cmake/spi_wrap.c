b9be531fdb7aaf28f23b5c42c4655bdd4205c981
wenxin.leong <wenxin.leong@infineon.com>
2022-03-08 13:17:02 +0800

    edit code
    restruct spi_wrap

--- linux/cmake/spi_wrap.c
+++ linux/cmake/spi_wrap.c
@@ -1,7 +1,18 @@
 #include "linux/spi/spi.h"
 #include "linux/tpm.h"
+#include "spi_wrap.h"
 
-extern int spi_sync_locked(struct spi_device *spi, struct spi_message *message)
+int spi_sync_locked(struct spi_device *spi, struct spi_message *message)
 {
     /* SPI wrapper! */
+}
+
+void spi_init()
+{
+
+}
+
+void spi_release()
+{
+
 }
